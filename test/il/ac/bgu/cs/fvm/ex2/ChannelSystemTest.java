bce0bff7f39a6b6cf7bb011843d4825c480e206f
geraw <gera.weiss@gmail.com>
2017-01-01 15:57:09 +0200

    Examples from Bohan Moed Bet

--- test/il/ac/bgu/cs/fvm/ex2/ChannelSystemTest.java
+++ test/il/ac/bgu/cs/fvm/ex2/ChannelSystemTest.java
@@ -1,9 +1,11 @@
 package il.ac.bgu.cs.fvm.ex2;
 
-import il.ac.bgu.cs.fvm.FvmFacade;
 import static il.ac.bgu.cs.fvm.util.CollectionHelper.map;
 import static il.ac.bgu.cs.fvm.util.CollectionHelper.p;
 import static il.ac.bgu.cs.fvm.util.CollectionHelper.seq;
+import static il.ac.bgu.cs.fvm.util.CollectionHelper.set;
+import static il.ac.bgu.cs.fvm.util.CollectionHelper.transition;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -11,83 +13,116 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import il.ac.bgu.cs.fvm.FvmFacade;
 import il.ac.bgu.cs.fvm.channelsystem.ChannelSystem;
 import il.ac.bgu.cs.fvm.examples.AlternatingBitProtocolBuilder;
+import il.ac.bgu.cs.fvm.programgraph.PGTransition;
+import il.ac.bgu.cs.fvm.programgraph.ProgramGraph;
 import il.ac.bgu.cs.fvm.transitionsystem.AlternatingSequence;
 import il.ac.bgu.cs.fvm.transitionsystem.TransitionSystem;
 import il.ac.bgu.cs.fvm.util.Pair;
 
 public class ChannelSystemTest {
 
+	FvmFacade fvmFacadeImpl = FvmFacade.createInstance();
 
 	@SuppressWarnings("unchecked")
 	@Test
-    public void debug() throws Exception {
+	public void debug() throws Exception {
+		TransitionSystem<Pair<List<String>, Map<String, Object>>, String, String> ts = fvmFacadeImpl
+				.transitionSystemFromChannelSystem(AlternatingBitProtocolBuilder.build());
 
-        FvmFacade fvmFacadeImpl = FvmFacade.createInstance();
-
-        TransitionSystem<Pair<List<String>, Map<String, Object>>, String, String> ts = fvmFacadeImpl.transitionSystemFromChannelSystem(AlternatingBitProtocolBuilder.build());
-
-		assertTrue(fvmFacadeImpl.isInitialExecutionFragment(ts, 
-				AlternatingSequence.of(
-						p(seq("snd_msg(0)", "off", "wait(0)"), map()),
-						"C!0",
-						p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C",seq(0)))),
-						"_tmr_on!|_tmr_on?",
-						p(seq("wait(0)", "on", "wait(0)"), map(p("C",seq(0)))),
-						"_timeout?|_timeout!",
-						p(seq("snd_msg(0)", "off", "wait(0)"), map(p("C",seq(0)))),
-						"C!0",
-						p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C",seq(0,0)))),
-						"C?y",
-						p(seq("set_tmr(0)", "off", "pr_msg(0)"), map(p("y",0),p("C",seq(0)))),
-						"",
-						p(seq("set_tmr(0)", "off", "snd_ack(0)"), map(p("y",0),p("C",seq(0)))),
-						"D!0",
-						p(seq("set_tmr(0)", "off", "wait(1)"), map(p("y",0),p("C",seq(0)),p("D",seq(0)))),
-						"C?y",
-						p(seq("set_tmr(0)", "off", "pr_msg(1)"), map(p("y",0),p("C",seq()),p("D",seq(0)))),
-						"",
-						p(seq("set_tmr(0)", "off", "wait(1)"), map(p("y",0),p("C",seq()),p("D",seq(0))))
+		assertTrue(fvmFacadeImpl.isInitialExecutionFragment(ts,
+				AlternatingSequence.of(p(seq("snd_msg(0)", "off", "wait(0)"), map()), "C!0",
+						p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0)))), "_tmr_on!|_tmr_on?",
+						p(seq("wait(0)", "on", "wait(0)"), map(p("C", seq(0)))), "_timeout?|_timeout!",
+						p(seq("snd_msg(0)", "off", "wait(0)"), map(p("C", seq(0)))), "C!0",
+						p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0, 0)))), "C?y",
+						p(seq("set_tmr(0)", "off", "pr_msg(0)"), map(p("y", 0), p("C", seq(0)))), "",
+						p(seq("set_tmr(0)", "off", "snd_ack(0)"), map(p("y", 0), p("C", seq(0)))), "D!0",
+						p(seq("set_tmr(0)", "off", "wait(1)"), map(p("y", 0), p("C", seq(0)), p("D", seq(0)))), "C?y",
+						p(seq("set_tmr(0)", "off", "pr_msg(1)"), map(p("y", 0), p("C", seq()), p("D", seq(0)))), "",
+						p(seq("set_tmr(0)", "off", "wait(1)"), map(p("y", 0), p("C", seq()), p("D", seq(0))))
 
 				)));
-        
-    }
+
+	}
 
 	@Test
-    public void different_pg_order() throws Exception {
+	public void different_pg_order() throws Exception {
+		ChannelSystem<String, String> cs = AlternatingBitProtocolBuilder.build();
 
-        FvmFacade fvmFacadeImpl = FvmFacade.createInstance();
+		TransitionSystem<Pair<List<String>, Map<String, Object>>, String, String> ts = fvmFacadeImpl
+				.transitionSystemFromChannelSystem(cs);
 
-        ChannelSystem<String, String> cs = AlternatingBitProtocolBuilder.build();
-
-		TransitionSystem<Pair<List<String>, Map<String, Object>>, String, String> ts = fvmFacadeImpl.transitionSystemFromChannelSystem(cs);
-	
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		AlternatingSequence<Pair<List<String>, Map<String, Object>>, String> seq = AlternatingSequence.of(
-                p(seq("snd_msg(0)", "off", "wait(0)"), map()),
-                "C!0",
-                p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0)))),
-                "_tmr_on!|_tmr_on?",
-                p(seq("wait(0)", "on", "wait(0)"), map(p("C", seq(0)))),
-                "_timeout?|_timeout!",
-                p(seq("snd_msg(0)", "off", "wait(0)"), map(p("C", seq(0)))),
-                "C!0",
-                p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0,0)))),
-                "C?y",
-                p(seq("set_tmr(0)", "off", "pr_msg(0)"), map(p("C", seq(0)),p("y",0))),
-                "",
-                p(seq("set_tmr(0)", "off", "snd_ack(0)"), map(p("C", seq(0)),p("y",0))),
-                "D!0",
-                p(seq("set_tmr(0)", "off", "wait(1)"), map(p("C", seq(0)),p("D", seq(0)),p("y",0))),
-                "C?y",
-                p(seq("set_tmr(0)", "off", "pr_msg(1)"), map(p("C", seq()),p("D", seq(0)),p("y",0))),
-                "",
-                p(seq("set_tmr(0)", "off", "wait(1)"), map(p("C", seq()),p("D", seq(0)),p("y",0)))
-        );
+				p(seq("snd_msg(0)", "off", "wait(0)"), map()), "C!0",
+				p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0)))), "_tmr_on!|_tmr_on?",
+				p(seq("wait(0)", "on", "wait(0)"), map(p("C", seq(0)))), "_timeout?|_timeout!",
+				p(seq("snd_msg(0)", "off", "wait(0)"), map(p("C", seq(0)))), "C!0",
+				p(seq("set_tmr(0)", "off", "wait(0)"), map(p("C", seq(0, 0)))), "C?y",
+				p(seq("set_tmr(0)", "off", "pr_msg(0)"), map(p("C", seq(0)), p("y", 0))), "",
+				p(seq("set_tmr(0)", "off", "snd_ack(0)"), map(p("C", seq(0)), p("y", 0))), "D!0",
+				p(seq("set_tmr(0)", "off", "wait(1)"), map(p("C", seq(0)), p("D", seq(0)), p("y", 0))), "C?y",
+				p(seq("set_tmr(0)", "off", "pr_msg(1)"), map(p("C", seq()), p("D", seq(0)), p("y", 0))), "",
+				p(seq("set_tmr(0)", "off", "wait(1)"), map(p("C", seq()), p("D", seq(0)), p("y", 0))));
 
-        assertTrue(fvmFacadeImpl.isInitialExecutionFragment(ts, seq));
+		assertTrue(fvmFacadeImpl.isInitialExecutionFragment(ts, seq));
 
-    }
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test10() throws Exception {
+		ProgramGraph<String, String> pg1 = fvmFacadeImpl.createProgramGraph();
+		
+		pg1.addLocation("l1");
+		pg1.addLocation("l2");
+
+		pg1.addInitialLocation("l1");
+	
+		pg1.addInitalization(seq("x:=0"));
+
+		pg1.addTransition(new PGTransition<>("l1", "true", "C?x", "l2"));
+		pg1.addTransition(new PGTransition<>("l2", "x != 0 && size(C)<5", "C!x", "l1"));
+
+		ProgramGraph<String, String> pg2 = fvmFacadeImpl.createProgramGraph();
+
+		pg2.addLocation("l1");
+		pg2.addLocation("l2");
+
+		pg2.addInitialLocation("l1");
+	
+		pg2.addInitalization(seq("y:=0","y:=1"));
+
+		pg2.addTransition(new PGTransition<>("l1", "size(C)<5", "C!y", "l2"));
+		pg2.addTransition(new PGTransition<>("l2", "true", "C?y", "l1"));
+		
+		
+		ChannelSystem<String, String> cs = new ChannelSystem<>(seq(pg1,pg2));
+		
+		TransitionSystem<Pair<List<String>, Map<String, Object>>, String, String> ts = fvmFacadeImpl
+				.transitionSystemFromChannelSystem(cs);
+		
+		
+		assertEquals(set(p(seq("l1", "l1"), map(p("x",0), p("y",1))), p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1)))), p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1)))), p(seq("l2", "l2"), map(p("x",1), p("y",1), p("C",seq()))), p(seq("l1", "l1"), map(p("x",1), p("y",1), p("C",seq()))), 
+		    p(seq("l1", "l1"), map(p("x",0), p("y",1), p("C",seq())))),ts.getStates());
+		assertEquals(set(p(seq("l1", "l1"), map(p("x",0), p("y",1)))),ts.getInitialStates());
+		assertEquals(set("C!x", "C!y", "C?y", "C?x"),ts.getActions());
+		assertEquals(set("C = []", "x = 1", "x = 0", "y = 1", "C = [1]"
+		    ),ts.getAtomicPropositions());
+		assertEquals(set(transition(p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1)))), "C?x", p(seq("l2", "l2"), map(p("x",1), p("y",1), p("C",seq())))), transition(p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1)))), "C?x", p(seq("l2", "l2"), map(p("x",1), p("y",1), p("C",seq())))), transition(p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1)))), "C?y", p(seq("l1", "l1"), map(p("x",0), p("y",1), p("C",seq())))), transition(p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1)))), "C?y", p(seq("l1", "l1"), map(p("x",1), p("y",1), p("C",seq())))), transition(p(seq("l1", "l1"), map(p("x",0), p("y",1))), "C!y", p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1))))), 
+		    transition(p(seq("l1", "l1"), map(p("x",0), p("y",1), p("C",seq()))), "C!y", p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1))))), transition(p(seq("l1", "l1"), map(p("x",1), p("y",1), p("C",seq()))), "C!y", p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1))))), transition(p(seq("l2", "l2"), map(p("x",1), p("y",1), p("C",seq()))), "C!x", p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1)))))),ts.getTransitions());
+		assertEquals(set("x = 0", "y = 1"), ts.getLabel(p(seq("l1", "l1"), map(p("x",0), p("y",1)))));
+		assertEquals(set("x = 1", "y = 1", "C = [1]"), ts.getLabel(p(seq("l1", "l2"), map(p("x",1), p("y",1), p("C",seq(1))))));
+		assertEquals(set("x = 0", "y = 1", "C = [1]"), ts.getLabel(p(seq("l1", "l2"), map(p("x",0), p("y",1), p("C",seq(1))))));
+		assertEquals(set("C = []", "x = 1", "y = 1"), ts.getLabel(p(seq("l2", "l2"), map(p("x",1), p("y",1), p("C",seq())))));
+		assertEquals(set("C = []", "x = 1", "y = 1"), ts.getLabel(p(seq("l1", "l1"), map(p("x",1), p("y",1), p("C",seq())))));
+		assertEquals(set("C = []", "x = 0", "y = 1"), ts.getLabel(p(seq("l1", "l1"), map(p("x",0), p("y",1), p("C",seq())))));
+
+
+
+	}
 
 }
