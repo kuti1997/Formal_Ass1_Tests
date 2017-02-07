17da7a1e6805e514ff2b43640f3f983b0e87838b
Gera Weiss <gera.weiss@gmail.com>
2017-02-07 08:45:49 +0200

    A more general return type for GNBA2NBA.

--- test/il/ac/bgu/cs/fvm/ex4/GNBACritSectionTest.java
+++ test/il/ac/bgu/cs/fvm/ex4/GNBACritSectionTest.java
@@ -12,46 +12,42 @@ import org.junit.Test;
 import il.ac.bgu.cs.fvm.FvmFacade;
 import il.ac.bgu.cs.fvm.automata.Automaton;
 import il.ac.bgu.cs.fvm.automata.MultiColorAutomaton;
-import il.ac.bgu.cs.fvm.util.Pair;
+import il.ac.bgu.cs.fvm.util.Util;
 
 public class GNBACritSectionTest {
 	FvmFacade fvmFacadeImpl = FvmFacade.createInstance();
-	
+
 	@Test
 	public void simpleTest() throws Exception {
 
 		MultiColorAutomaton<String, String> mulAut = getMCAut();
-		Automaton<Pair<String, Integer>, String> aut = fvmFacadeImpl.GNBA2NBA(mulAut);
+		Automaton<?, String> aut = fvmFacadeImpl.GNBA2NBA(mulAut);
 		assertEquals(aut, getExpected());
 
 	}
 
-	MultiColorAutomaton<String,String> getMCAut() {
-		MultiColorAutomaton<String,String> aut = new MultiColorAutomaton<String,String>();
-		HashSet<String> none = new HashSet<String>();
-		HashSet<String> crit1 = new HashSet<String>(asList("crit1"));
-		HashSet<String> crit2 = new HashSet<String>(asList("crit2"));
-		HashSet<String> both = new HashSet<String>(asList("crit1", "crit2"));
+	MultiColorAutomaton<String, String> getMCAut() {
+		MultiColorAutomaton<String, String> aut = new MultiColorAutomaton<String, String>();
 
-		aut.addTransition("s0", crit2, "s2");
-		aut.addTransition("s0", crit1, "s1");
+		aut.addTransition("s0", set("crit2"), "s2");
+		aut.addTransition("s0", set("crit1"), "s1");
 
 		// True transitions
-		for (Set<String> s : asList(none, crit1, crit2, both)) {
+		for (Set<String> s : Util.powerSet(set("crit1", "crit2"))) {
 			aut.addTransition("s0", new HashSet<String>(s), "s0");
 			aut.addTransition("s1", new HashSet<String>(s), "s0");
 			aut.addTransition("s2", new HashSet<String>(s), "s0");
 		}
 
-		aut.setInitial  ("s0");
+		aut.setInitial("s0");
 		aut.setAccepting("s1", 1);
 		aut.setAccepting("s2", 2);
 
 		return aut;
 	}
 
-	Automaton<String,String> getExpected() {
-		Automaton<String,String> aut = new Automaton<>();
+	Automaton<String, String> getExpected() {
+		Automaton<String, String> aut = new Automaton<>();
 
 		Set<String> none = set();
 		Set<String> crit1 = set("crit1");
@@ -66,7 +62,7 @@ public class GNBACritSectionTest {
 		// True transitions
 		for (Set<String> s : asList(none, crit1, crit2, both)) {
 			aut.addTransition("s01", s, "s01");
-			aut.addTransition("s11", s, "s02");			
+			aut.addTransition("s11", s, "s02");
 			aut.addTransition("s02", s, "s02");
 			aut.addTransition("s21", s, "s01");
 			aut.addTransition("s12", s, "s02");
